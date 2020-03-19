88f64ad6a4389bdd88cf1de93cf0ef0d9540b3a7
Stanley Fang <stanleysfang92@gmail.com>
2020-03-18 23:32:41 -0700

    fixed remote github reference; added more useful commands

--- git/create_repository.sh
+++ git/create_repository.sh
@@ -10,10 +10,16 @@ cp /home/sfang/windows/gitlab/stanleysfang/code_reference/.gitignore . # copy a 
 git add -A
 git commit -m "initial commit"
 
-#### Remote Repository ####
+#### Add Remote Repository ####
 # Make sure to first create a blank repository on gitlab/github.
 git remote add origin git@gitlab.com:stanleysfang/code_reference.git
 git push origin master
 
-git remote add github git@gitlab.com:stanleysfang/code_reference.git
+git remote add github git@github.com:stanleysfang/code_reference.git
 git push github master
+
+#### Remove Remote Repository ####
+git remote rm github
+
+#### Useful Commands ####
+git remote -v
