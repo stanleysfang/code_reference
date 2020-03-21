3a682794f77ec7bc7c113d9fbd9c8f03323a693e
Stanley Fang <stanleysfang92@gmail.com>
2020-03-21 10:38:42 -0700

    coded Loader object; modified time_partitioning

--- gcp/google_cloud_bigquery/Loader.py
+++ gcp/google_cloud_bigquery/Loader.py
@@ -16,59 +16,39 @@ class Loader:
         self.client = bigquery.Client(project=self.run_project)
         self.job_history=[]
     
-    def config_job(self, destination_table=None, overwrite=True, partition_field=None, dry_run=False):
-        job_config = bigquery.QueryJobConfig()
+    def config_job(self, schema, overwrite=True, time_partitioning=False, partition_field=None):
+        job_config = bigquery.LoadJobConfig()
         
-        job_config.use_legacy_sql = False
-        job_config.destination = destination_table
-        job_config.dry_run = dry_run
+        job_config.schema = [bigquery.SchemaField(name, field_type) for name, field_type in schema]
         
         if overwrite:
             job_config.write_disposition = 'WRITE_TRUNCATE'
         else:
             job_config.write_disposition = 'WRITE_APPEND'
         
-        if destination_table and partition_field:
+        # The only supported partition type is "DAY" which is the default.
+        if time_partitioning:
             job_config.time_partitioning = bigquery.table.TimePartitioning(field=partition_field)
         
         return job_config
     
-    def load_df(self, query_str, destination_table=None, overwrite=True, partition_field=None, dry_run=False):
+    def load_df(self, df, destination_table, schema, overwrite=True, time_partitioning=False, partition_field=None):
         """
         Loads a pandas dataframe to BigQuery and returns a handler to the job.
         
         Args:
-            query_str: query string
-            destination_table: the table that the query will write to (e.g. 'project_id.dataset_id.table_id')
+            df: pandas dataframe
+            destination_table: the table that the job will write to (e.g. 'project_id.dataset_id.table_id') (for time partition tables, 'project_id.dataset_id.table_id$YYYYMMDD')
+            schema: a list of tuples that contains name and field_type
             overwrite: a boolean where True will overwrite destination table and False will append to it
+            time_partitioning: a boolean where True will load a partition table
             partition_field: the column to partition the table on (column can be TIMESTAMP or DATE data type)
-            dry_run: a boolean indicating whether the query should be a dry run
         
         Returns:
-            google.cloud.bigquery.job.QueryJob: a handler to the query job
+            google.cloud.bigquery.job.LoadJob: a handler to the load job
         """
-        job_config = self.config_job(destination_table, overwrite, partition_field, dry_run)
-        query_job = self.client.query(query_str, job_config=job_config)
-        self.job_history.append(query_job)
+        job_config = self.config_job(schema, overwrite, time_partitioning, partition_field)
+        load_job = self.client.load_table_from_dataframe(df, destination_table, job_config=job_config)
+        self.job_history.append(load_job)
         
-        return query_job
-
-job_config = bigquery.LoadJobConfig()
-
-job_config.write_disposition = 'WRITE_TRUNCATE'
-job_config.schema = [
-    bigquery.SchemaField('province_state', 'STRING'),
-    bigquery.SchemaField('country_region', 'STRING'),
-    bigquery.SchemaField('latitude', 'FLOAT64'),
-    bigquery.SchemaField('longitude', 'FLOAT64')    
-] + [bigquery.SchemaField(dt_col, 'INT64') for dt_col in dt_cols]
-
-for df, metric in [(confirmed, 'confirmed'), (deaths, 'deaths'), (recovered, 'recovered')]:
-    load_job = client.load_table_from_dataframe(
-        df,
-        destination='stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{metric}_raw'.format(metric=metric),
-        job_config=job_config
-    )
-    load_job.result()
-
-
+        return load_job
--- gcp/google_cloud_bigquery/QueryRunner.py
+++ gcp/google_cloud_bigquery/QueryRunner.py
@@ -16,7 +16,7 @@ class QueryRunner:
         self.client = bigquery.Client(project=self.run_project)
         self.job_history=[]
     
-    def config_job(self, destination_table=None, overwrite=True, partition_field=None, dry_run=False):
+    def config_job(self, destination_table=None, overwrite=True, time_partitioning=False, partition_field=None, dry_run=False):
         job_config = bigquery.QueryJobConfig()
         
         job_config.use_legacy_sql = False
@@ -28,28 +28,28 @@ class QueryRunner:
         else:
             job_config.write_disposition = 'WRITE_APPEND'
         
-        if destination_table and partition_field:
+        if destination_table and time_partitioning:
             job_config.time_partitioning = bigquery.table.TimePartitioning(field=partition_field)
         
         return job_config
     
-    def run_query(self, query_str, destination_table=None, overwrite=True, partition_field=None, dry_run=False):
+    def run_query(self, query_str, destination_table=None, overwrite=True, time_partitioning=False, partition_field=None, dry_run=False):
         """
         Executes a query and returns a handler to the job.
         
         Args:
             query_str: query string
-            destination_table: the table that the query will write to (e.g. 'project_id.dataset_id.table_id')
+            destination_table: the table that the query will write to (e.g. 'project_id.dataset_id.table_id') (for time partition tables, 'project_id.dataset_id.table_id$YYYYMMDD')
             overwrite: a boolean where True will overwrite destination table and False will append to it
+            time_partitioning: a boolean where True will write the query to a partition table
             partition_field: the column to partition the table on (column can be TIMESTAMP or DATE data type)
             dry_run: a boolean indicating whether the query should be a dry run
         
         Returns:
             google.cloud.bigquery.job.QueryJob: a handler to the query job
         """
-        job_config = self.config_job(destination_table, overwrite, partition_field, dry_run)
+        job_config = self.config_job(destination_table, overwrite, time_partitioning, partition_field, dry_run)
         query_job = self.client.query(query_str, job_config=job_config)
         self.job_history.append(query_job)
         
         return query_job
-
