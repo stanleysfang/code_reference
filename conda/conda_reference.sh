3dedde2f2662eaf8149b26755969bb617bdf264c
Stanley Fang <stanleysfang92@gmail.com>
2020-03-22 02:16:44 -0700

    moved pandas data into example_data directory; updated pandas_reference.py; added conda_reference.sh

---
+++ conda/conda_reference.sh
@@ -0,0 +1,14 @@
+
+#### conda reference ####
+
+# create env
+conda create -y --name query python=3.7.0 pip
+
+# remove env
+conda remove -y --name query --all
+
+# list env
+conda env list
+
+# activate env
+source activate query
