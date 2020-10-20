6fba3cff62d63449c7576d62a3353f275bdacab1
Stanley Fang <stanleysfang92@gmail.com>
2020-10-19 21:02:10 -0700

    added jupyter_notebook_reference.sh

---
+++ jupyter_notebook/jupyter_notebook_reference.sh
@@ -0,0 +1,7 @@
+
+#### Common Commands ####
+
+# First install python package ipykernel
+jupyter kernelspec list
+jupyter kernelspec remove -f <kernel-name>
+python -m ipykernel install --name <kernel-name> --display-name "Python 3.7.0 (<kernel-name>)" --user
